93b8f4d43f20d9d435b59c7c531ede4cfe181661
lgavlich <lgavlich@gmail.com>
2021-10-22 22:27:25 +0300

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,40 +1,45 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import { createPortal } from "react-dom";
+//import { createPortal } from "react-dom";
 
-const modalRoot = document.querySelector("#modal-root");
+//const modalRoot = document.querySelector("#modal-root");
 
 export default function Modal({ onClose, modalImage: { modalImage } }) {
   useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.code === "Escape") {
-        onClose();
-      }
-    };
+    //const handleKeyDown = (e) => {
+    //if (e.code === "Escape") {
+    //onClose();
+    //}
+    //};
     window.addEventListener("keydown", handleKeyDown);
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [onClose]);
+  });
 
-  const handleBackdropClick = (e) => {
-    if (e.currentTarget === e.target) {
+  const handleKeyDown = (e) => {
+    if (e.code === "Escape") {
       onClose();
     }
   };
 
-  return createPortal(
-    <div className="Overlay" onClick={handleBackdropClick}>
+  //const handleBackdropClick = (e) => {
+  //if (e.currentTarget === e.target) {
+  //onClose();
+  //}
+  //};
+
+  return (
+    <div className="Overlay" onClick={onClose}>
       <div className="Modal">
         <img src={modalImage} alt="" />
       </div>
-    </div>,
-    modalRoot
+    </div>
   );
 }
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
-  modalImage: PropTypes.string.isRequired,
+  modalImage: PropTypes.object.isRequired,
 };
